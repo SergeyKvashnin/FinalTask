7fd90476ae22f5de909b32c794bc5b626b68ae78
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-04-01 21:34:46 +0300

    task v.1

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//программа формирующая из данного массива, массив из строк, длинна которых меньше либо равна 3 символам.
+Console.Clear();
+string[] mass1 = new string[5] {"743", "23", "hello", "world", "tod"};
+string[] mass2 = new string[mass1.Length];
+void First (string[] mass1, string[] mass2)
+{
+    int count = 0;
+    for (int i = 0; i < mass1.Length; i++)
+    {
+    if(mass1[i].Length <= 3)
+        {
+        mass2[count] = mass1[i];
+        count++;
+        }
+    }
+}
+
+
